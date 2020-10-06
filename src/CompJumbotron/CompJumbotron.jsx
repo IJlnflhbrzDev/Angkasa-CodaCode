aea2041377de3179d7f874473e79ab699e1841cf
Ijlnflhbrz <ijlnflhbrz@gmail.com>
2020-10-06 20:45:34 +0700

    Memper Baiki Layout Mobile Responsiv

--- src/CompJumbotron/CompJumbotron.jsx
+++ src/CompJumbotron/CompJumbotron.jsx
@@ -9,7 +9,7 @@ const BoxComponent = () => {
   return (
     <div id="CompBoxJumbotrons">
       <Row>
-        <Col lg="3" sm="6" xs="6" >
+        <Col xl="3" sm="6" xs="12" >
           <Card id="CardCompBoxIcon" body>
             <div className="text-center">
               <box-icon color="red" animation="tada-hover" size="lg" name='html5' type='logo' ></box-icon>
@@ -17,7 +17,7 @@ const BoxComponent = () => {
             <Button>HTML</Button>
           </Card>
         </Col>
-        <Col lg="3" sm="6" xs="6">
+        <Col lg="3" sm="6" xs="12">
           <Card id="CardCompBoxIcon" body>
             <div className="text-center">
               <box-icon color="blue" animation="tada-hover" size="lg" name='css3' type='logo' ></box-icon>
@@ -25,7 +25,7 @@ const BoxComponent = () => {
             <Button>CSS</Button>
           </Card>
         </Col>
-        <Col lg="3" sm="6" xs="6">
+        <Col lg="3" sm="6" xs="12">
           <Card id="CardCompBoxIcon" body>
             <div className="text-center">
               <box-icon color="yellowgreen" animation="tada-hover" size="lg" name='javascript' type='logo' ></box-icon>
@@ -33,7 +33,7 @@ const BoxComponent = () => {
             <Button>JAVASCRIPT</Button>
           </Card>
         </Col>
-        <Col lg="3" sm="6" xs="6">
+        <Col lg="3" sm="6" xs="12">
           <Card id="CardCompBoxIcon" body>
             <div className="text-center">
               <box-icon color="purple" animation="tada-hover" size="lg" name='bootstrap' type='logo' ></box-icon>
@@ -53,20 +53,21 @@ class CompJumbotron extends React.Component {
     return (
 
 
-      <Jumbotron id="CompJumbotron" className="d-flex justify-content-center align-items-center flex-column">
-
+      <Jumbotron id="CompJumbotron" className="d-flex align-items-center flex-column">
         <Row>
-          <Col col="md-12">
-            <h1 className="font-weight-bold text-white">Belajar Budi Daya Code Bersama DeveloperKids</h1>
-            <hr className="my-2 mt-3" color="text-primary" />
-            <p className="lead">Jangan tunggu nanti, tanamlah sekarang biar cepat panen!</p>
-            <p className="lead">
-              <Button className="border-fuchsia">Learn More!</Button>
-            </p>
-          </Col>
+          <div id="CompBoxJumbotrons_Content">
+            <Col>
+              <h1 className="font-weight-bold text-white ">Belajar Budi Daya Code Bersama DeveloperKids</h1>
+              <hr className="my-2 mt-3" color="text-primary" />
+              <p className="lead">Jangan tunggu nanti, tanamlah sekarang biar cepat panen!</p>
+              <p className="lead">
+                <Button className="border-fuchsia">Learn More!</Button>
+              </p>
+            </Col>
 
+          </div>
         </Row>
-        <Container className="mt-5">
+        <Container>
           <BoxComponent />
         </Container>
       </Jumbotron>
--- src/ComponentService/CompService.jsx
+++ src/ComponentService/CompService.jsx
@@ -8,52 +8,58 @@ class CompService extends React.Component {
   render() {
     return (
       <div>
-        <Row>
-          <Col lg="3" md="6" xs="6">
-            <Card>
-              <CardHeader className="text-center bg-danger">Header</CardHeader>
-              <CardBody>
-                <CardTitle>Special Title Treatment</CardTitle>
-                <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
-                <Button>Go somewhere</Button>
-              </CardBody>
-              <CardFooter>Footer</CardFooter>
-            </Card>
-          </Col>
-          <Col lg="3" md="6" xs="6">
-            <Card>
-              <CardHeader className="text-center bg-danger">Header</CardHeader>
-              <CardBody>
-                <CardTitle>Special Title Treatment</CardTitle>
-                <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
-                <Button>Go somewhere</Button>
-              </CardBody>
-              <CardFooter>Footer</CardFooter>
-            </Card>
-          </Col>
-          <Col lg="3" md="6" xs="6">
-            <Card>
-              <CardHeader className="text-center bg-danger">Header</CardHeader>
-              <CardBody>
-                <CardTitle>Special Title Treatment</CardTitle>
-                <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
-                <Button>Go somewhere</Button>
-              </CardBody>
-              <CardFooter>Footer</CardFooter>
-            </Card>
-          </Col>
-          <Col lg="3" md="6" xs="6">
-            <Card>
-              <CardHeader className="text-center bg-danger">Header</CardHeader>
-              <CardBody>
-                <CardTitle>Special Title Treatment</CardTitle>
-                <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
-                <Button>Go somewhere</Button>
-              </CardBody>
-              <CardFooter>Footer</CardFooter>
-            </Card>
-          </Col>
-        </Row>
+        <Container>
+          <Row>
+
+            <Col lg="3" md="6" xs="12 mb-2 mt-4 text-center">
+              <Card>
+
+                <CardHeader className="text-center bg-danger">HTML</CardHeader>
+                <CardBody>
+                  <CardTitle>Special Title Treatment</CardTitle>
+                  <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+                  <Button className="btn-outline-primary">Go somewhere</Button>
+                </CardBody>
+
+              </Card>
+            </Col>
+            <Col lg="3" md="6" xs="12 mb-2 mt-4 text-center">
+              <Card>
+                <CardHeader className="text-center bg-danger">CSS</CardHeader>
+                <CardBody>
+                  <CardTitle>Special Title Treatment</CardTitle>
+                  <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+                  <Button className="btn-outline-primary">Go somewhere</Button>
+                </CardBody>
+
+              </Card>
+            </Col>
+            <Col lg="3" md="6" xs="12 mb-2 mt-4 text-center">
+              <Card>
+                <CardHeader className="text-center bg-danger">JAVASCRIPT</CardHeader>
+                <CardBody>
+                  <CardTitle>Special Title Treatment</CardTitle>
+                  <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+                  <Button className="btn-outline-primary">Go somewhere</Button>
+                </CardBody>
+
+              </Card>
+            </Col>
+            <Col lg="3" md="6" xs="12 mb-2 mt-4 text-center">
+              <Card>
+                <CardHeader className="text-center bg-danger">REACT JS</CardHeader>
+                <CardBody>
+                  <CardTitle>Special Title Treatment</CardTitle>
+                  <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+                  <Button className="btn-outline-primary">Go somewhere</Button>
+                </CardBody>
+
+
+              </Card>
+
+            </Col>
+          </Row>
+        </Container>
       </div>
     )
   }
